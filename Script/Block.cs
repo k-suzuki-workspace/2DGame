789a7e89e0607f57f6fdbc90c8d3c889093970c4
k.suzuki <suzuken@Mac.local>
2018-05-28 00:28:11 +0900

    作業中

--- Script/Block.cs
+++ Script/Block.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Block : MonoBehaviour {
 
 	public float m_speed;
+	private bool m_isGround;
 
 	void Start(){
-		m_speed = -5f;
+		Debug.LogError("start");
+		this.gameObject.SetActive(true);
+		m_speed = 5f;
+		m_isGround = false;
 	}
 
 	void Update () {
 		// 移動_
-		this.gameObject.transform.localPosition += new Vector3(m_speed, 0, 0);
-
+		if (m_isGround){
+			this.gameObject.transform.localPosition += new Vector3(-m_speed, 0, 0);
+		}
 		// TODO:画面端に行ったらDestoryする_
 
 	}
+
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if(LayerMask.LayerToName(collision.gameObject.layer) == "Ground"){
+			m_isGround = true;
+			Debug.LogError("m_isGround = " + m_isGround);
+		}
+	}
+
+	//void OnCollisionExit2D(Collision2D collision){
+	//	if (LayerMask.LayerToName(collision.gameObject.layer) == "Ground"){
+	//		m_isGround = false;
+	//		Debug.LogError("m_isGround = " + m_isGround);
+	//	}
+	//}
 
 }
--- Script/ObjectGenerator.cs
+++ Script/ObjectGenerator.cs
@@ -2,23 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class ObjectGenerator : MonoBehaviour {
 
 	[SerializeField]
 	private GameObject m_blockObj;
 
-	private float m_count;
-	public float m_interval = 10f;
+	private float m_count = 200f;
+	public float m_interval = 200f;
 
 
 	void Update()
 	{
 		// 一定間隔ごとにブロックを生成する_
 		m_count++;
-		Debug.LogError("m_count = " + m_count);
 		if(m_count > m_interval){
-			Instantiate(m_blockObj, m_blockObj.transform);
-			m_interval = 0;
+			Instantiate(m_blockObj, this.transform);
+			m_count = 0;
 		}
 	}
 
